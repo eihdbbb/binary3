da96aee1a6a7ba49111155aa3dd4abe51cea894f
eihdbbb <eihdbbb@i.ua>
2017-05-29 15:27:47 +0300

    first commit

---
+++ Program/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Beasts;
+using Menu;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TopMenu menu = new TopMenu();
+
+            BeastsZoo beasts = new BeastsZoo();
+            DataSource dataSource = new DataSource();
+            dataSource.GetBeasts( beasts );
+
+            beasts.PrintAll();
+            do
+            {
+                
+            } while (menu.MakeChoice(beasts) != 0);
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
